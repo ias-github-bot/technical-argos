99113a96bfe9bb3a22f1c3a33621a510d0f5766a
DaragonIAS <daniel.aragon@ias.com.co>
2025-07-15 18:18:19 -0500

    menu_and_views

--- my_hostel/__manifest__.py
+++ my_hostel/__manifest__.py
@@ -8,10 +8,9 @@
     "website": "http://www.serpentcs.com",
     "license": "AGPL-3",
     "depends": ["base"],
-    # This data files will be loaded at the installation (commented because file is not added in this example)
-    # 'data': [
-    #     'views/views.xml'
-    # ],
+    "data": [
+        "views/hostel.xml",
+	],
     # This demo data files will be loaded if db initialize with demo data (commented because file is not added in this example)
     # 'demo': [
     #     'demo.xml'
