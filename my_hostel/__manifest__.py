cbbff263e8e19db0854f2fd43d816c895da91580
DaragonIAS <daniel.aragon@ias.com.co>
2025-07-15 18:17:28 -0500

    new_addon_file_structure

--- my_hostel/__manifest__.py
+++ my_hostel/__manifest__.py
@@ -10,7 +10,8 @@
     "depends": ["base"],
     # This data files will be loaded at the installation (commented because file is not added in this example)
     # 'data': [
-    #     'views.xml'
+    #     'views/views.xml'.
+    #     'reports/report.xml'
     # ],
     # This demo data files will be loaded if db initialize with demo data (commented because file is not added in this example)
     # 'demo': [
