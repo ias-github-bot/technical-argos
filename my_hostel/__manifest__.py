305e8004b61398f3e5fe4cbb0f0441c4c80b044e
DaragonIAS <daniel.aragon@ias.com.co>
2025-07-16 11:44:00 -0500

    12_qweb_report

--- my_hostel/__manifest__.py
+++ my_hostel/__manifest__.py
@@ -22,6 +22,9 @@ Efficiently manage the entire residential facility in the school
         "views/hostel_room_stages_views.xml",
         "views/hostel_student.xml",
         "views/hostel_categ.xml",
+        "reports/hostel_room_detail_report_template.xml",
+        "reports/hostel_room_detail_report.xml",
+
     ],
     # This demo data files will be loaded if db initialize with demo data (commented because file is not added in this example)
     # 'demo': [
