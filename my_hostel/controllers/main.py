d4dbfff37d2c0bb6285fb2969ef91b80e26d33a9
DaragonIAS <daniel.aragon@ias.com.co>
2025-07-16 11:50:23 -0500

    13_static_resource

--- my_hostel/controllers/main.py
+++ my_hostel/controllers/main.py
@@ -4,16 +4,13 @@ from odoo.http import request
 
 class Main(http.Controller):
 
-    @http.route('/my_hostel/student_details', type='http', auth='none')
-    def student_details(self, student_id):
-        record = request.env['hostel.student'].sudo().browse(int(student_id))
-        return u'<html><body><h1>%s</h1>Room No: %s' % (
-            record.name,
-            str(record.room_id.room_no) or 'none',
-        )
-
-    @http.route("/my_hostel/student_details/<model('hostel.student'):student>",  
-            type='http', auth='none')
-    def student_details_in_path(self, book):
-        return self.student_details(student.id)
+    @http.route('/demo_page', type='http', auth='none')
+    def students(self):
+        image_url = '/my_hostel/static/src/image/odoo.png'
+        html_result = """<html>
+            <body>
+                <img src="%s"/>
+            </body>
+        </html>""" % image_url
+        return html_result
 
