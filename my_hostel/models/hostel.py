1165be18a04ece2e352e66de5079f8f21eb9f1c6
DaragonIAS <daniel.aragon@ias.com.co>
2025-07-16 11:35:30 -0500

    10_field_access

--- my_hostel/models/hostel.py
+++ my_hostel/models/hostel.py
@@ -9,3 +9,4 @@ class Hostel(models.Model):
     hostel_code = fields.Char(string="Code", required=True)
     street = fields.Char('Street')
     street2 = fields.Char('Street2')
+    notes = fields.Text(groups="my_hostel.group_hostel_manager")
